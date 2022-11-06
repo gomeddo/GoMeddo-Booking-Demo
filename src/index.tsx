4ac899d98d0e132127bd0001a23f97b45c7914e5
Tom van der Lee <t.vanderlee@gen25.com>
2022-11-06 20:18:36 +0100

    Create custom element

--- src/index.tsx
+++ src/index.tsx
@@ -14,18 +14,55 @@ import 'dayjs/locale/fr'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 
-dayjs.extend(isSameOrBefore)
-dayjs.extend(localizedFormat)
+function render (rootElement: HTMLElement | ShadowRoot): ReactDOM.Root {
+  dayjs.extend(isSameOrBefore)
+  dayjs.extend(localizedFormat)
 
-const root = ReactDOM.createRoot(
-  document.getElementById('sales-appointment') as HTMLElement
-)
+  const root = ReactDOM.createRoot(rootElement)
 
-const queryClient = new QueryClient()
-root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
-)
+  const queryClient = new QueryClient()
+
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  )
+
+  return root
+}
+
+if (process.env.REACT_APP_CUSTOM_ELEMENT === '1') {
+  class SalesAppointment extends HTMLDivElement {
+    private readonly loadingPromise: Promise<void>
+
+    constructor () {
+      super()
+
+      const assetManifestUrl = `${process.env.PUBLIC_URL ?? ''}/asset-manifest.json`
+
+      this.loadingPromise = fetch(assetManifestUrl)
+        .then(async response => await response.json())
+        .then(({ files }) => {
+          const stylesheet = document.createElement('link')
+          stylesheet.rel = 'stylesheet'
+          stylesheet.href = files['main.css']
+
+          const shadowRoot = this.attachShadow({ mode: 'closed' })
+          render(shadowRoot)
+
+          shadowRoot.prepend(stylesheet)
+        })
+    }
+  }
+
+  window.customElements.define('sales-appointment', SalesAppointment, {
+    extends: 'div'
+  })
+} else {
+  const root = document.getElementById('sales-appointment')
+  if (root !== null) {
+    render(root)
+  }
+}
